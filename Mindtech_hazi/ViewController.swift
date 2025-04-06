8778cb105da9842ffa661bc3c13da7745a05a818
Zoltan Toth <tothzoltan01181996@gmail.com>
2025-04-06 15:24:22 +0200

    init work

---
+++ Mindtech_hazi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mindtech_hazi
+//
+//  Created by Tóth Zoltán on 2025. 04. 06..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
